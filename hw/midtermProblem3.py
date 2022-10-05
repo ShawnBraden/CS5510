d562a007f3c00a0051a63451a5e2062d924f3811
jacobread <jread8888@gmail.com>
2022-10-05 14:34:23 -0600

    Finished part b and c of problem 3

--- hw/midtermProblem3.py
+++ hw/midtermProblem3.py
@@ -1,46 +1,60 @@
 import math
 import random
+from turtle import distance
 from motion import motionArm
 
-STARTING_D = [1, 0.5, 1, 0, 0, 0.2]
-STARTING_THETA = [math.radians(-90), 0, 0, math.radians(-90), math.radians(90), math.radians(40)]
-END_POINT = [1.2, 0.8, 0.5]
-THRESHOLD = .001
+# theta1 = −90deg, d2 = 0.5m, d3 = 1.0m, theata4 = −90deg, theta5 = 90deg, theta6 = 40deg, d6 = 0.2m
+A_STARTING_DISTANCE = [1, 0.5, 1, 0, 0, 0.2]
+A_STARTING_THETA = [math.radians(-90), 0, 0, math.radians(-90), math.radians(90), math.radians(40)]
 
-def getRandom(quench):
+# θ1 = 0deg, d2 = 0.2m, d3 = 0.3m, θ4 = −90deg, θ5 = 90deg, θ6 = 40deg, d6 = 0.2m
+B_STARTING_DISTANCE = [1, 0.2, 0.3, 0, 0, 0.2]
+B_STARTING_THETA = [math.radians(0), 0, 0, math.radians(-90), math.radians(90), math.radians(40)]
+
+END_POINT = [1.2, 0.8, 0.5]
+THRESHOLD = .01
+
+def getRandom(quench, problem):
   # theta6 doesn't change so we only need to worry about 2, 4, and 5
-  theta1 = (random.random() - 0.5) * quench
+  if problem == "c":
+    theta1 = (random.random() - 0.5) * (quench * .01)
+    quench = quench * .1
+  else:
+    theta1 = (random.random() - 0.5) * quench
+
   theta4 = (random.random() - 0.5) * quench
   theta5 = (random.random() - 0.5) * quench
   thetas = [theta1, theta4, theta5]
 
   # d2 and d3 also need to change
-  d2 = (random.random() - 0.5) * quench
-  d3 = (random.random() - 0.5) * quench
-  dList = [d2, d3]
+  if problem == "b":
+    distanceQuench = quench * .01
+  else:
+    distanceQuench = quench
 
-  return thetas, dList
+  d2 = (random.random() - 0.5) * distanceQuench
+  d3 = (random.random() - 0.5) * distanceQuench
+  distances = [d2, d3]
 
+  return thetas, distances
 
-def computeJointAngles():
-  # theta1 = −90deg, d2 = 0.5m, d3 = 1.0m, theata4 = −90deg, theta5 = 90deg, theta6 = 40deg, d6 = 0.2m
-  arm = motionArm(STARTING_D)
-  
-  currentPoint = arm.calculateMotionArmNotCorrected(STARTING_THETA)
-  currentTheta = STARTING_THETA
-  currentDistance = STARTING_D
+def computeTravel(startingTheta, startingDistance, problem):
+  arm = motionArm(startingDistance)
+  currentPoint = arm.calculateMotionArmNotCorrected(startingTheta)
+  currentTheta = startingTheta
+  currentDistance = startingDistance
 
-  n = 100000
+  n = 1000000
   for i in range(n):
     # quench this down so that we are have less variance over time
     if i < (.3 * n):
-      thetaDiff, distanceDiff = getRandom(.3)
+      thetaDiff, distanceDiff = getRandom(.3, problem)
     elif i < (.5 * n):
-      thetaDiff, distanceDiff = getRandom(.1)
+      thetaDiff, distanceDiff = getRandom(.1, problem)
     elif i < (.7 * n):
-      thetaDiff, distanceDiff = getRandom(.08)
+      thetaDiff, distanceDiff = getRandom(.08, problem)
     else:
-      thetaDiff, distanceDiff = getRandom(.05)
+      thetaDiff, distanceDiff = getRandom(.05, problem)
     
     newThetas = [
       currentTheta[0]+thetaDiff[0],
@@ -58,16 +72,32 @@ def computeJointAngles():
       currentPoint = newPoint
       currentTheta = newThetas
       currentDistance = newDistance
-      print("Improved Point: ", newPoint)
 
     if (math.dist(currentPoint, END_POINT) < THRESHOLD):
       return currentPoint, currentTheta, currentDistance
-      
+
 
 def main():
-  finalPoint, finalThatas, finalDistances = computeJointAngles()
-  print("\nFinal Point (x, y, z): ", finalPoint)
-  print("Final Thetas (degrees): theta1 = %2d, theta4 = %2d, theta5 = %2d" % (math.degrees(finalThatas[0]), math.degrees(finalThatas[3]), math.degrees(finalThatas[4])))
-  print("Final Distances (meters): d2 = %1f, d3 = %1f\n" % (finalDistances[1], finalDistances[2]))
-  
+  # problem a
+    finalPoint, finalThatas, finalDistances = computeTravel(A_STARTING_THETA, A_STARTING_DISTANCE, "a")
+    print("Problem A")
+    print("Final Point (x, y, z): ", finalPoint)
+    print("Final Thetas (degrees): θ1 = %2d, θ4 = %2d, θ5 = %2d, θ6 = %2d" % (math.degrees(finalThatas[0]), math.degrees(finalThatas[3]), math.degrees(finalThatas[4]), math.degrees(finalThatas[5])))
+    print("Final Distances (meters): d2 = %2f, d3 = %2f, d6 = %2f\n" % (finalDistances[1], finalDistances[2], finalDistances[5]))
+    
+  # problem b
+    finalPoint, finalThatas, finalDistances = computeTravel(B_STARTING_THETA, B_STARTING_DISTANCE, "b")
+    print("\nProblem B")
+    print("Final Point (x, y, z): ", finalPoint)
+    print("Final Thetas (degrees): θ1 = %2d, θ4 = %2d, θ5 = %2d, θ6 = %2d" % (math.degrees(finalThatas[0]), math.degrees(finalThatas[3]), math.degrees(finalThatas[4]), math.degrees(finalThatas[5])))
+    print("Final Distances (meters): d2 = %2f, d3 = %2f, d6 = %2f\n" % (finalDistances[1], finalDistances[2], finalDistances[5]))
+    
+
+  # problem c
+    finalPoint, finalThatas, finalDistances = computeTravel(B_STARTING_THETA, B_STARTING_DISTANCE, "c")
+    print("\nProblem C")
+    print("Final Point (x, y, z): ", finalPoint)
+    print("Final Thetas (degrees): θ1 = %2d, θ4 = %2d, θ5 = %2d, θ6 = %2d" % (math.degrees(finalThatas[0]), math.degrees(finalThatas[3]), math.degrees(finalThatas[4]), math.degrees(finalThatas[5])))
+    print("Final Distances (meters): d2 = %2f, d3 = %2f, d6 = %2f\n" % (finalDistances[1], finalDistances[2], finalDistances[5]))
+    
 main()
