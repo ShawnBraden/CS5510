e040a43677a6224a3c354a667bf358b0f351d7bf
jacobread <jread8888@gmail.com>
2022-10-17 12:54:50 -0600

    started changing the cart pole code to help solve problem 4 and 5

--- Midterm/midtermProblem4.py
+++ Midterm/midtermProblem4.py
@@ -1,21 +1,48 @@
 import gym
 import numpy as np
 
-from gym import utils
-from gym.envs.mujoco import MuJocoPyEnv
-from gym.spaces import Box
 
-
-# Part C
+# # Part C
 env = gym.make("CartPole-v1", render_mode="human")
 observation, info = env.reset(seed=42)
 
-for _ in range(1000):
-    action = env.action_space.sample()
+# action is 0 or 1 (left or right)
+action = env.action_space.sample()
+
+for _ in range(1000):  
+  # determine the directon we need to move base on the pole's angle
+    if (observation[2] > 0):  
+        action = 1
+    else:
+        action = 0
+
     observation, reward, terminated, truncated, info = env.step(action)
 
+    print("Obervation: ", observation)
+    # print("Reward: ", reward)
+    # print("Terminated: ", terminated)
+    # print("Truncated: ", truncated)
+    # print("Info: ", info)
+    
     if terminated or truncated:
         observation, info = env.reset()
+        # print("\n")
 
-# env_screen = env.render(mode = 'rgb_array')
 env.close()
+
+class increaseRewardWrapper(gym.RewardWrapper):
+    def __init__(self, env):
+        super().__init__(env)
+    
+    def reward(self, rew):
+        rew += 0.5
+        return rew
+
+class decreaseRewardWrapper(gym.RewardWrapper):
+    def __init__(self, env):
+        super().__init__(env)
+    
+    def reward(self, rew):
+        rew -= 0.5
+        return rew
+
