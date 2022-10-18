e8bbe29bc560a7ecb71d476e16f91ccf0347d714
Trey Crowther <trey.crowther@asirobots.com>
2022-10-18 10:43:31 -0600

    Actual controller perfection

--- Midterm/midtermProblem4.py
+++ Midterm/midtermProblem4.py
@@ -8,22 +8,26 @@ observation, info = env.reset(seed=42)
 # action is 0 or 1 (left or right)
 action = env.action_space.sample()
 
-for _ in range(1000):  
+for i in range(1000):  
 
-  # determine the directon we need to move base on the pole's angle
-    if (observation[2] > 0.05):  
+    # determine the directon we need to move base on the pole's angle, current position and pole angular velocity
+    cartPosition = observation[0]
+    poleAngle = observation[2]
+    poleAngularVelocity = observation[3]
+    if (poleAngle > 0.15):
         action = 1
-    elif (observation[2] < -0.05):
+    elif (poleAngle < -0.15):
         action = 0
-    elif (observation[2] > -0.05 and observation[2] < 0 and action == 1):
+    elif (poleAngle > -0.15 and poleAngle < 0 and action == 1):
         action = 0
-    elif (observation[2] < 0.05 and observation[2] > 0 and action == 0):
+    elif (poleAngle < 0.15 and poleAngle > 0 and action == 0):
         action = 1
-    if(observation[3] > 0.5):
+    if (poleAngularVelocity > 0.25):
         action = 1
-    elif(observation[3] < -0.5):
+    elif (poleAngularVelocity < -0.25):
         action = 0
-
+    if(i % 7 == 0 and abs(cartPosition) > .1):
+        action = 1 if cartPosition > 0 else 0
     observation, reward, terminated, truncated, info = env.step(action)
 
     print("Observation: ", observation)
