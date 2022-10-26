f002a69c5050bb14ea596d09d2de43f3553ed997
Jared-Wasson <>
2022-10-26 10:26:57 -0600

    finished 8b

---
+++ Midterm/problem8/READMEPROBELM8.md
@@ -0,0 +1,4 @@
+    problem 8.b instructions
+1. cd problem 8
+2. python launch.py
+3. watch all ten classifications running for 10 seconds each
---
+++ Midterm/problem8/launch.py
@@ -0,0 +1,12 @@
+import os 
+
+os.system('python yolo_opencv.py --image crop_1.jpeg --config crop_weed.cfg --weights crop_weed_detection.weights --classes yolov3.txt')
+os.system('python yolo_opencv.py --image crop_2.jpeg --config crop_weed.cfg --weights crop_weed_detection.weights --classes yolov3.txt')
+os.system('python yolo_opencv.py --image crop_3.jpeg --config crop_weed.cfg --weights crop_weed_detection.weights --classes yolov3.txt')
+os.system('python yolo_opencv.py --image crop_4.jpeg --config crop_weed.cfg --weights crop_weed_detection.weights --classes yolov3.txt')
+os.system('python yolo_opencv.py --image weed_1.jpeg --config crop_weed.cfg --weights crop_weed_detection.weights --classes yolov3.txt')
+os.system('python yolo_opencv.py --image weed_2.jpeg --config crop_weed.cfg --weights crop_weed_detection.weights --classes yolov3.txt')
+os.system('python yolo_opencv.py --image weed_3.jpeg --config crop_weed.cfg --weights crop_weed_detection.weights --classes yolov3.txt')
+os.system('python yolo_opencv.py --image weed_4.jpeg --config crop_weed.cfg --weights crop_weed_detection.weights --classes yolov3.txt')
+os.system('python yolo_opencv.py --image weed_5.jpeg --config crop_weed.cfg --weights crop_weed_detection.weights --classes yolov3.txt')
+os.system('python yolo_opencv.py --image weed_6.jpeg --config crop_weed.cfg --weights crop_weed_detection.weights --classes yolov3.txt')
